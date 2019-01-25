2493b5b82282360c7b44e8a9c4154cdb1cfb1175
pjay79 <praveen.jayarajan@gmail.com>
2019-01-26 00:13:15 +1100

    Add react-native-push-notifications and PubNub setup

--- App.js
+++ App.js
@@ -7,15 +7,63 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PubNubReact from 'pubnub-react';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.header}>Fantasy Football Alerts</Text>
-    <Text style={styles.subHeader}>Push Notifications</Text>
-  </View>
-);
+const PushNotification = require('react-native-push-notification');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: 'YOUR_PUBNUB_PUBLISH_KEY_HERE',
+      subscribeKey: 'YOUR_PUBNUB_SUBSCRIBE_KEY_HERE',
+    });
+    this.pubnub.init(this);
+    PushNotification.configure({
+      // Called when Token is generated.
+      onRegister(token) {
+        console.log('TOKEN:', token);
+        if (token.os === 'ios') {
+          this.pubnub.push.addChannels({
+            channels: ['notifications'],
+            device: token.token,
+            pushGateway: 'apns',
+          });
+          // Send iOS notify from debug console: {"pn_apns":{"aps":{"alert":"Hello World."}}}
+        } else if (token.os === 'android') {
+          this.pubnub.push.addChannels({
+            channels: ['notifications'],
+            device: token.token,
+            pushGateway: 'gcm', // apns, gcm, mpns
+          });
+          // Send Android notify from debug console: {"pn_gcm":{"data":{"message":"Hello World."}}}
+        }
+      },
+      // Something not working?
+      // See: https://support.pubnub.com/support/solutions/articles/14000043605-how-can-i-troubleshoot-my-push-notification-issues-
+      // Called when a remote or local notification is opened or received.
+      onNotification(notification) {
+        console.log('NOTIFICATION:', notification);
+        // Do something with the notification.
+        // Required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+        // notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      // ANDROID: GCM or FCM Sender ID
+      senderID: 'sender-id',
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Fantasy Football Alerts</Text>
+        <Text style={styles.subHeader}>Push Notifications</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -36,5 +84,3 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
 });
-
-export default App;
