03f7a51103a498af7716dcba451555df25e154dc
pjay79 <praveen.jayarajan@gmail.com>
2019-01-26 12:35:54 +1100

    Update PushNotification configuration

--- App.js
+++ App.js
@@ -9,49 +9,62 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PushNotification from 'react-native-push-notification';
 import PubNubReact from 'pubnub-react';
 
-const PushNotification = require('react-native-push-notification');
-
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.pubnub = new PubNubReact({
-      publishKey: 'YOUR_PUBNUB_PUBLISH_KEY_HERE',
-      subscribeKey: 'YOUR_PUBNUB_SUBSCRIBE_KEY_HERE',
+  componentDidMount() {
+    const pubnub = new PubNubReact({
+      publishKey: 'pub-c-8f812f40-d146-4617-839a-71b242e219d2',
+      subscribeKey: 'sub-c-49dd4b46-2055-11e9-b712-2656c4b29a42',
     });
-    this.pubnub.init(this);
     PushNotification.configure({
       // Called when Token is generated.
       onRegister(token) {
         console.log('TOKEN:', token);
         if (token.os === 'ios') {
-          this.pubnub.push.addChannels({
-            channels: ['notifications'],
-            device: token.token,
-            pushGateway: 'apns',
-          });
-          // Send iOS notify from debug console: {"pn_apns":{"aps":{"alert":"Hello World."}}}
+          pubnub.push.addChannels(
+            {
+              channels: ['notifications'],
+              device: token.token,
+              pushGateway: 'apns',
+            },
+            (status) => {
+              if (status.error) {
+                console.log('Operation failed: ', status);
+              } else {
+                console.log('Operation done!');
+              }
+            },
+          );
+          // Send to iOS from debug console: {"pn_apns":{"aps":{"alert":"Hello World."}}}
         } else if (token.os === 'android') {
-          this.pubnub.push.addChannels({
-            channels: ['notifications'],
-            device: token.token,
-            pushGateway: 'gcm', // apns, gcm, mpns
-          });
-          // Send Android notify from debug console: {"pn_gcm":{"data":{"message":"Hello World."}}}
+          pubnub.push.addChannels(
+            {
+              channels: ['notifications'],
+              device: token.token,
+              pushGateway: 'gcm', // apns, gcm, mpns
+            },
+            (status) => {
+              if (status.error) {
+                console.log('Operation failed: ', status);
+              } else {
+                console.log('Operation done!');
+              }
+            },
+          );
+          // Send to Android from debug console: {"pn_gcm":{"data":{"message":"Hello World."}}}
         }
       },
-      // Something not working?
-      // See: https://support.pubnub.com/support/solutions/articles/14000043605-how-can-i-troubleshoot-my-push-notification-issues-
       // Called when a remote or local notification is opened or received.
       onNotification(notification) {
-        console.log('NOTIFICATION:', notification);
+        console.log('Alert:', notification);
         // Do something with the notification.
         // Required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
         // notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
       // ANDROID: GCM or FCM Sender ID
-      senderID: 'sender-id',
+      senderID: '887580580040',
     });
   }
 
